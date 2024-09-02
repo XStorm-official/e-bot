e8ea070be9b856ab413ae73f93d6a24876fd419e
XStorm <112734582+XStorm-official@users.noreply.github.com>
2024-09-02 20:41:42 +0200

    Delete install1.sh

--- install1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-curl -sSL https://get.docker.com/ | CHANNEL=stable bash
-sudo systemctl enable --now docker
-rm /etc/default/grub
-cd /etc/default/
-wget "https://raw.githubusercontent.com/XStorm-official/e-bot/main/grub"
-reboot
