e46069e0665c55c1d247c459a5ffe5bfc85ac572
XStorm <112734582+XStorm-official@users.noreply.github.com>
2024-09-22 20:20:43 +0200

    Update pterodactyl.sh

--- pterodactyl.sh
+++ pterodactyl.sh
@@ -53,7 +53,7 @@ else
 fi
 
 echo "Installation des paquets nécessaires..."
-if sudo apt install -y software-properties-common curl apt-transport-https ca-certificates gnupg apache2; then
+if sudo apt install -y software-properties-common curl apt-transport-https ca-certificates gnupg apache2 cron; then
     success_message "Paquets nécessaires installés"
 else
     error_message "Échec de l'installation des paquets"
